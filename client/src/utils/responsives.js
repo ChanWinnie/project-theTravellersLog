95d0f197bd22100d14b8d43c19342c05f7dc7c37
ChanWinnie <chanwinnie.wc@gmail.com>
2021-04-26 22:17:12 -0400

    responsive util added

---
+++ client/src/utils/responsives.js
@@ -0,0 +1,27 @@
+export const size = {
+  smallPhone: 415,
+  tablet: 768,
+  desktop: 1024,
+  // largeDesktop: 1440,
+  largeDesktop: 1025,
+};
+
+export const device = {
+  smallPhone: `(max-width: ${size.smallPhone}px)`,
+  tablet: `(max-width: ${size.tablet}px)`,
+  desktop: `(max-width: ${size.desktop}px)`,
+  largeDesktop: `(min-width: ${size.largeDesktop}px)`,
+};
+
+export const onSmallPhoneMediaQuery = (): string => `
+    @media ${device.smallPhone}
+  `;
+export const onTabletMediaQuery = (): string => `
+    @media ${device.tablet}
+  `;
+export const onDesktopMediaQuery = (): string => `
+    @media ${device.desktop}
+  `;
+export const onLargeDesktopMediaQuery = (): string => `
+    @media ${device.largeDesktop}
+  `;
